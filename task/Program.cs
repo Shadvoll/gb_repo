b7d267ccfd9455bf2ffc63fa13e93c29b55042bd
shaukat <shadvoll@gmail.com>
2023-03-02 13:56:35 +0300

    Dotnet program task created

---
+++ task/Program.cs
@@ -0,0 +1,47 @@
+﻿int GetNum(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+string[] GetStrings(int n)
+{
+    string[] strs = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i+1} строку: ");
+        strs[i] = Console.ReadLine();
+    }
+    return strs;
+}
+
+string[] FilterStrings(string[] strs)
+{
+    int n = strs.Length;
+    int m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (strs[i].Length <= 3)
+        {
+            m++;
+        }
+    }
+    string[] result = new string[m];
+    m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (strs[i].Length <= 3)
+        {
+            result[m++] = strs[i];
+        }
+    }
+    return result;
+}
+
+int n = GetNum("Введите количество строк: ");
+string[] inputStrings = GetStrings(n);
+string[] resultStrings = FilterStrings(inputStrings);
+Console.WriteLine("Строки, длины которых меньше 3: ");
+for(int i = 0 ; i < resultStrings.Length; i++){
+    Console.Write(resultStrings[i] + " ");
+}
